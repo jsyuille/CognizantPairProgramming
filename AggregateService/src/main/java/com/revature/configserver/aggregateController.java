70f3f669d9f0c1782b1bfb0161053f05dbd50f82
Jesse Yuille <Jesseyuille@yahoo.com>
2020-03-06 16:41:22 -0500

    Jesse Yuille Bader Juma

--- AggregateService/src/main/java/com/revature/configserver/aggregateController.java
+++ AggregateService/src/main/java/com/revature/configserver/aggregateController.java
@@ -1,5 +1,8 @@
 package com.revature.configserver;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,18 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class aggregateController {
 
+	@Autowired
+	ExperimentClient expClient;
 	
-	GetMapping("/api/experiment/**")
-	public ResponseEntity<SpeedResult>() {
-		//Gets user input
-		//parses string for distance
-		//converts distance using calculator service
-		return ResponseEntity.ok(SpeedResult);
+	@Autowired
+	ConversionClient conversionClient;
+	
+	@GetMapping("/api/experiment/**")
+	public ResponseEntity<Object> getExperiment(String subject, Double unit) {
+		Object e = expClient.getExperiment(subject);
+		e = conversionClient.convert(e, unit);
+		
+		return ResponseEntity.ok(e);
 	}
 	
-	PostMapping("/api/experiment")
-	public ResponseEntity<SpeedResult> () {
-		//return object
-		return ResponseEntity.ok(SpeedResult);
-	}
 }
--- projectRepsitory/src/main/java/com/revature/configserver/ProjectRepsitoryApplication.java
+++ projectRepsitory/src/main/java/com/revature/configserver/ProjectRepsitoryApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
 public class ProjectRepsitoryApplication {
 
 	public static void main(String[] args) {
